0bbff5cfbee810b5ded575cc6c79fa2ac600fcc4
Shubhodeep <banerjeeshubho98@gmail.com>
2023-12-12 00:10:41 +0530

    updates

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,10 @@
 import Input from "@/components/Input";
-import {signIn} from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { useCallback, useState } from "react";
 import axios from "axios";
-import {useRouter} from 'next/router'
-import {FcGoogle} from 'react-icons/fc'
-import {FaGithub} from 'react-icons/fa'
+import { useRouter } from 'next/router'
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
 
 
 const Auth = () => {
@@ -47,7 +47,7 @@ const Auth = () => {
             })
 
             login();
-        } catch(error) {
+        } catch (error) {
             console.log(error);
         }
 
@@ -96,8 +96,15 @@ const Auth = () => {
                         </div>
                         <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 rounded-md w-full text-white mt-10 hover:bg-red-800 transform transition duration-500 hover:scale-110 ">{variant === 'login' ? 'Login' : 'Sign up'}</button>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-                            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"><FcGoogle size={30}/></div>
-                            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"><FaGithub size={30}/></div>
+                            <div
+                                onClick={() => signIn('google', { callbackUrl: '/' })}
+                            className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={30} />
+                            </div>
+
+                            <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FaGithub size={30} />
+                            </div>
                         </div>
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? "First time using NetFlix ?" : 'Already have an account ? '}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-    <p className="text-5xl font-serif font-bold text-white">Hello, Welcome to Netflix Clone</p>
-  </div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="text-6xl font-serif font-bold text-white">Hello, Welcome to Netflix 😃</p>
+      <button onClick={() => signOut()} className='text-white w-40 h-10 bg-red-500 hover:bg-yellow-400 rounded-lg transform transition duration-500 hover:scale-110 text-2xl font-serif'>Logout</button>
+    </div>
 
   )
 }
