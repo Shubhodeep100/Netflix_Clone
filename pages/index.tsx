24a65d4ea25de8c1f9a0378798cbdddf6db22584
Shubhodeep <banerjeeshubho98@gmail.com>
2023-12-27 12:58:52 +0530

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentUser';
+import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 export async function getServerSideProps(context: NextPageContext) {
@@ -21,11 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
-      <Billboard/>
+      <Billboard />
+      <div className='pb-40'>
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   )
 }
