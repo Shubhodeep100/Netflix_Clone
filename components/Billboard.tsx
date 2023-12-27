d2a429dd9de5ae90b698290b26870398c49c57b7
Shubhodeep <banerjeeshubho98@gmail.com>
2023-12-27 20:39:29 +0530

    updates

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -8,7 +8,6 @@ const Billboard = () => {
     return (
         <div className="relative h-[56.25vw]">
             <video className="w-full h-[56.25vw] object-cover brightness-[50%]" autoPlay muted loop poster={data?.thumbnailUrl} src={data?.videoUrl}>
-
             </video>
             <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
                 <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold  drop-shadow-xl">
