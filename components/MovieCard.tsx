e9693f9391048c43aee154c859207f1e9e9545bd
Shubhodeep <banerjeeshubho98@gmail.com>
2023-12-29 01:26:46 +0530

    updates

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
+import { useRouter } from "next/router";
+
 interface MovieCardProps {
     data: Record<string, any>;
 }
@@ -8,11 +10,15 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
+
+    const rouetr = useRouter();
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
             <img
-                className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]"
-                src={data.thumbnailUrl} alt="Thumbnail" />
+                className="w-full h-[12vw] object-cover transition duration shadow-xl rounded-md cursor-pointer group-hover:opacity-90 sm:group-hover:opacity-0 delay-300"
+                src={data.thumbnailUrl}
+                alt="Thumbnail"
+            />
             <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
                 <img
                     className="cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]"
@@ -21,7 +27,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
                     <div className="flex flex-row items-center gap-3">
                         <div
                             className="flex cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full justify-center items-center transition hover:bg-neutral-300"
-                            onClick={() => { }}>
+                            onClick={() => rouetr.push(`/watch/${data?.id}`)}>
                             <BsFillPlayFill size={20} />
                         </div>
                         <FavoriteButton movieId={data?.id} />
