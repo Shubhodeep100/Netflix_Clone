a3ed7ee58484d2390c217c74e7209df1c740a221
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-14 00:54:14 +0530

    updates

--- pages/api/movies/[movieId].ts
+++ pages/api/movies/[movieId].ts
@@ -1,32 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prismadb from '@/lib/prismadb';
+
+import prismadb from "@/lib/prismadb";
 import serverAuth from "@/lib/serverAuth";
 
-export default async function handler(req: NextApiRequest , res: NextApiResponse){
-    if(req.method !== 'GET'){
-        return res.status(405).end();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method !== "GET") {
+      return res.status(405).end();
     }
 
-    try {
-        await serverAuth(req, res);
-        const {movieId} = req.query;
-        if(typeof movieId !== 'string') {
-            throw new Error('Invalid ID');
-        }
+    await serverAuth(req, res);
 
-        const movie = await prismadb.movie.findUnique({
-            where: {
-                id: movieId
-            }
-        })
+    const { movieId } = req.query;
 
-        if(!movie){
-            throw new Error('Invalid ID');
-        }
+    if (!movieId) throw new Error("Invalid ID");
 
-        return res.status(200).json(movie);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).end();
-    }
+    if (typeof movieId !== "string") throw new Error("Invalid ID");
+
+    const movie = await prismadb.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (!movie) throw new Error("Invalid ID");
+
+    return res.status(200).json(movie);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(400).end();
+  }
 }
